c1dafc210a53efad197e2f1c9cb7e96bf14a3822
LipaMar <48134943+LipaMar@users.noreply.github.com>
2020-01-31 19:36:21 +0100

    Add Recipe & Ingredients

--- src/main/Recipe.java
+++ src/main/Recipe.java
@@ -3,7 +3,6 @@ package main;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
